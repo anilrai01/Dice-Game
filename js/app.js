cdde52797e44a8fb3ade2a477cd288922252493c
anuraghazra <hazru.anurag@gmail.com>
2019-04-29 22:55:12 +0530

    refactor variables

--- js/app.js
+++ js/app.js
@@ -19,154 +19,166 @@ to his GLOBAL score. After that, it's the next player's turn
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying, prevDice = [0,0], winningScore;
-// console.log(gamePlaying);
+var scores, roundScore, activePlayer, gamePlaying, prevDice = [0, 0], winningScore;
+
+function _get(id) {
+  return document.querySelector(id);
+}
+
+var DOMbtnNew = _get('.btn-new');
+var DOMbtnRoll = _get('.btn-roll');
+var DOMbtnHold = _get('.btn-hold');
+
+var DOMPlayer0 = _get('.player-0');
+var DOMPlayer1 = _get('.player-1');
+
+var DOMDice1 = _get('.dice1');
+var DOMDice2 = _get('.dice2');
+
+var DOMName0 = _get('#name-0');
+var DOMName1 = _get('#name-1');
+
+var DOMScored0 = _get('#scored-0');
+var DOMScored1 = _get('#scored-1');
+
+var DOMTotalScored0 = _get('#totalScore-0');
+var DOMTotalScored1 = _get('#totalScore-1');
 
 gameInit();
 
-//Setting up Event Handler for button roll with Anonymous Funtion
-document.querySelector('.btn-roll').addEventListener('click', function(){
+function ButtonRoll() {
 
-    if(gamePlaying) {
+  if (gamePlaying) {
 
-    var dice = [0,0];
-        // 1. Random Number
+    var dice = [0, 0];
+    // 1. Random Number
     dice[0] = Math.floor(Math.random() * 6) + 1;
     dice[1] = Math.floor(Math.random() * 6) + 1;
 
 
-    // console.log('Active Player = ' + activePlayer);
-    if((prevDice[activePlayer] === dice[0] || prevDice[activePlayer] === dice[1])||(dice[0] === 6 && dice[2] === 6)){
-        prevDice[activePlayer] = 0;
-        // document.getElementById('scored-' + activePlayer).textContent = 0;
-        document.getElementById('totalScore-' + activePlayer).textContent = 0;
-        nextPlayer();
-    } else{
+    if ((prevDice[activePlayer] === dice[0] || prevDice[activePlayer] === dice[1]) || (dice[0] === 6 && dice[2] === 6)) {
+      prevDice[activePlayer] = 0;
+      // document.getElementById('scored-' + activePlayer).textContent = 0;
+      _get('#totalScore-' + activePlayer).textContent = 0;
+      nextPlayer();
+    } else {
 
-        // 2. Display the result
-    var diceDOM1 = document.querySelector('.dice1');
-    var diceDOM2 = document.querySelector('.dice2');
+      // 2. Display the result
+      DOMDice1.style.visibility = 'visible';
+      DOMDice2.style.visibility = 'visible';
 
-    diceDOM1.style.visibility = 'visible';
-    diceDOM2.style.visibility = 'visible';
+      DOMDice1.src = 'img/dice-' + dice[0] + '.png';
+      DOMDice2.src = 'img/dice-' + dice[1] + '.png';
 
-    diceDOM1.src = 'img/dice-' + dice[0] + '.png';
-    diceDOM2.src = 'img/dice-' + dice[1] + '.png';
+      // console.log(dice);
 
-    // console.log(dice);
-
-    // 3. Update the round score if the rolled number was not 1
-    if(dice[0] !== 1 && dice[1] !== 1){
+      // 3. Update the round score if the rolled number was not 1
+      if (dice[0] !== 1 && dice[1] !== 1) {
         //Add Score
         roundScore += dice[0] + dice[1];
         //Showing in UI
         document.querySelector('#scored-' + activePlayer).textContent = roundScore;
-    } else {
+      } else {
         // Next Player turn
         nextPlayer();
-    }
+      }
 
     }
 
     (dice[0] === 6 || dice[1] === 6) ? prevDice[activePlayer] = 6 : prevDice[activePlayer] = 0;
     // prevDice[activePlayer] = dice;
 
-    }
-    
-});
+  }
 
+}
 
-// Setting up Event Handler for Button hold
-document.querySelector('.btn-hold').addEventListener('click', function(){
-    
-    if(gamePlaying){
+function ButtonHold() {
 
-        //Add current scored to Global Score
+  if (gamePlaying) {
+
+    //Add current scored to Global Score
     scores[activePlayer] += roundScore;
-    
+
     //Update UI
-    document.getElementById('totalScore-' + activePlayer).textContent = scores[activePlayer];
+    _get('#totalScore-' + activePlayer).textContent = scores[activePlayer];
 
     // Check if the player won the game
-    var input =  document.getElementById('setGoal').value;
+    var input = _get('#setGoal').value;
 
-    //Type Coersion
-    //Undefined, 0, null or "" are coerced to false
-    //Anything else is coerced to true
-    if(input){
-        winningScore = input;
-    } else{
-        winningScore = 100;
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
     }
 
-    // console.log(typeof x);
-    // console.log(x);
-
-    if(scores[activePlayer] >= winningScore){
-        document.getElementById('name-'+activePlayer).textContent = 'Winner !';
-        document.querySelector('.player-'+ activePlayer).classList.remove('active');
-        document.querySelector('.player-'+ activePlayer).classList.add('winner');
-        gamePlaying = false;
-        // console.log(gamePlaying);
-
-    }else{
-        // Next Player Turn
-        nextPlayer();
+    if (scores[activePlayer] >= winningScore) {
+      _get('#name-' + activePlayer).textContent = 'Winner !';
+      _get('.player-' + activePlayer).classList.remove('active');
+      _get('.player-' + activePlayer).classList.add('winner');
+      gamePlaying = false;
+    } else {
+      // Next Player Turn
+      nextPlayer();
     }
 
-    }
-    
-});
+  }
 
+}
+
+
+//Setting up Event Handler for button roll with Anonymous Funtion
+DOMbtnRoll.addEventListener('click', ButtonRoll);
+// Setting up Event Handler for Button hold
+DOMbtnHold.addEventListener('click', ButtonHold);
 // Setting up new game button
-document.querySelector('.btn-new').addEventListener('click', gameInit);
+DOMbtnNew.addEventListener('click', gameInit);
 
 
 
-function nextPlayer(){
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-    
-    zeroScored();
-     0
-    //Changing Active State
-    document.querySelector('.player-0').classList.toggle('active');
-    document.querySelector('.player-1').classList.toggle('active');
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  zeroScored();
+
+  //Changing Active State
+  DOMPlayer0.classList.toggle('active');
+  DOMPlayer1.classList.toggle('active');
 }
 
-function gameInit(){
-    scores = [0,0];
-    roundScore = 0;
-    activePlayer = 0;
+function gameInit() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-    gamePlaying = true;
-    // console.log(gamePlaying);
+  gamePlaying = true;
+  // console.log(gamePlaying);
 
-    zeroScored();
-    zeroTotal();
+  zeroScored();
+  zeroTotal();
 
-    document.querySelector('.dice1').style.visibility = 'hidden';
-    document.querySelector('.dice2').style.visibility = 'hidden';
+  DOMDice1.style.visibility = 'hidden';
+  DOMDice2.style.visibility = 'hidden';
 
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
+  DOMName0.textContent = 'Player 1';
+  DOMName1.textContent = 'Player 2';
 
-    document.querySelector('.player-0').classList.remove('active');
-    document.querySelector('.player-1').classList.remove('active');
-    document.querySelector('.player-0').classList.add('active');
+  DOMPlayer0.classList.remove('active');
+  DOMPlayer1.classList.remove('active');
+  DOMPlayer0.classList.add('active');
 
-    document.querySelector('.player-0').classList.remove('winner');
-    document.querySelector('.player-1').classList.remove('winner');
-    
-    
+  DOMPlayer0.classList.remove('winner');
+  DOMPlayer1.classList.remove('winner');
+
+
 }
 
-function zeroScored(){
-    document.getElementById('scored-0').textContent = 0;
-    document.getElementById('scored-1').textContent = 0;
+function zeroScored() {
+  DOMScored0.textContent = 0;
+  DOMScored1.textContent = 0;
 }
 
-function zeroTotal(){
-    document.getElementById('totalScore-0').textContent = 0;
-    document.getElementById('totalScore-1').textContent = 0;
+function zeroTotal() {
+  DOMTotalScored0.textContent = 0;
+  DOMTotalScored1.textContent = 0;
 }
